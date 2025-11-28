04e84803f140dc6283fc3e5684e7a2ce187622b4
Soonoh <chk0ndanger@gmail.com>
2025-11-28 10:28:35 +0900

    Fix engine root path for manual app initializer

--- manual_app/config/initializers/tool_engine.rb
+++ manual_app/config/initializers/tool_engine.rb
@@ -5,7 +5,7 @@ require 'pathname'
 require 'ruby_llm'
 require 'application_tool'
 
-engine_root = Pathname.new(__dir__).join('..', '..').expand_path
+engine_root = Pathname.new(__dir__).join('..', '..', '..').expand_path
 $LOAD_PATH.unshift(engine_root.join('app/lib').to_s) unless $LOAD_PATH.include?(engine_root.join('app/lib').to_s)
 $LOAD_PATH.unshift(engine_root.join('app/services').to_s) unless $LOAD_PATH.include?(engine_root.join('app/services').to_s)
 
